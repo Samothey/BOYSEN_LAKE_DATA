890f7a51c84cad5548d845076274486364e75b3f
Samothey <spena5@uwyo.edu>
2025-01-15 15:08:54 -0700

    nonsense

---
+++ CODE/Boysenwatershed.R
@@ -0,0 +1,15 @@
+library(sf)
+library(tidyverse)
+
+getwd()
+# Verify the file path
+shapefile_path <- "DATA/Continental_US_from_NHDplus_v2-2"
+
+# Load the shapefile into R as an sf object
+watershed_sf <- st_read(shapefile_path)
+
+ggplot(watershed_sf) +
+  geom_sf()
+
+
+ggsave('Figures/boysenwatershed.png')
