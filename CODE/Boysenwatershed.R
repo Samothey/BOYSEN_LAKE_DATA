4bdcd4b8571f990c51909c720d3ba08ef8e4c291
Samantha Pena <spena5@uwyo.edu>
2025-01-17 16:43:14 -0700

    created all figures!

--- CODE/Boysenwatershed.R
+++ CODE/Boysenwatershed.R
@@ -1,5 +1,11 @@
 library(sf)
 library(tidyverse)
+# Install required libraries if not already installed
+if (!require("sf")) install.packages("sf")
+if (!require("ggplot2")) install.packages("ggplot2")
+
+library(sf)
+library(ggplot2)
 
 getwd()
 # Verify the file path
@@ -14,5 +20,19 @@ ggplot(watershed_sf) +
 
 ggsave('Figures/boysenwatershed.png')
 
+ggplot(data = watershed_sf) +
+  geom_sf(fill = "lavenderblush3", color = "lavenderblush4") +  # Static colors
+  labs(title = "Wind River Drainage") +
+  theme_void() +  # Removes all gridlines and axes
+  theme(plot.title = element_text(hjust = 0.5, size = 16))
 
-##HELP##
+
+# now I have created the shapefile, but want to add some color and personality to it.
+
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5, size = 16))
+
+
+### ATTEMPT TO MERGE 2 WATERSHED DELINATIONS.. 
+
+
